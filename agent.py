83a7d8d9ff6c2b841cab3cc0cdcd5004a0bfc7cd
JustAltF4 <35852992+JustAltF4@users.noreply.github.com>
2019-04-08 21:13:02 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,16 +1,18 @@
 
+# represents trading user
 class Agent():
   def __init__(self, name, balance, display_name):
-    self.name = name
+    self.name = name # discord user id
     self.balance = balance
-    self.display_name = display_name
+    self.display_name = display_name # discord name
     self.ready = False
-    self.owned_stocks = {}
+    self.owned_stocks = {} # how many of each stock the agent owns
     self.is_afk = False
 
+  # display all owned stocks
   def get_stock_str(self):
     output = self.display_name + ":\n"
     for key, value in self.owned_stocks.items():
-      if value > 0:
+      if value > 0: # only display stocks if the agent owns some
         output += key.capitalize() + ": " + str(value) + "\n"
     return output
