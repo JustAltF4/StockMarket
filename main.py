47e3c638a0bfc7d73125cf3fdb59b8c185681199
JustAltF4 <35852992+JustAltF4@users.noreply.github.com>
2019-04-08 21:12:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ from discord.ext import commands
 
 stock_market = None
 
+news_count = 4
+
 def init():
   global stock_market
   news.load_news()
 
-  stock_market = stocks.gen_stock_market(3)
+  stock_market = stocks.gen_stock_market(news_count) # initialises stock prices
 
 def tick():
   global stock_market
-  stock_market.tick()
+  stock_market.tick() # completes a turn
 
 # bot
 
@@ -26,85 +28,98 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
   init()
 
+# clears a channel of messages (for refreshing news/stocks/balances)
 async def clear(channel):
   async for msg in client.logs_from(channel):
     await client.delete_message(msg)
 
+# template
 @client.command(pass_context = True)
 async def test(ctx):
   await client.send_message(ctx.message.channel, "Hello")
 
+# set change multiplier - changes how much stocks fluctuate from news
 @client.command(pass_context = True)
 async def setcm(ctx, val):
   if not ctx.message.author.server_permissions.administrator:
     return
   news.change_mul = float(val)
 
+# re-initialises the stock market
 @client.command(pass_context = True)
 async def reset(ctx):
-  if not ctx.message.author.server_permissions.administrator:
+  if not ctx.message.author.server_permissions.administrator: # only for admins
     return
   init()
 
+# sets self to afk
 @client.command(pass_context = True)
 async def afk(ctx):
   stock_market.afk_agent(ctx.message.author.id, True)
 
+# sets self to not afk
 @client.command(pass_context = True)
 async def unafk(ctx):
   stock_market.afk_agent(ctx.message.author.id, False)
 
+# set other user to afk by user id
 @client.command(pass_context = True)
 async def setafk(ctx, name):
   if not ctx.message.author.server_permissions.administrator:
     return
   stock_market.afk_agent(name, True)
 
+# set other user to not afk by user id
 @client.command(pass_context = True)
 async def setunafk(ctx, name):
   if not ctx.message.author.server_permissions.administrator:
     return
   stock_market.afk_agent(name, True)
 
+# buy a stock
 @client.command(pass_context = True)
 async def buy(ctx, stock, amount):
   amount = int(amount)
-  if stock in stocks.stock_data.keys():
+  if stock in stocks.stock_data.keys(): # if they entered a valid stock
     bal = stock_market.get_agent_balance(ctx.message.author.id)
-    if bal >= stock_market.stock_prices[stock] * amount:
-      stock_market.buy(ctx.message.author.id, stock.lower(), amount)
-      await balance_redisplay()
-      await display_stocks(ctx.message.channel, ctx.message.author.id)
+    if bal >= stock_market.stock_prices[stock] * amount: # if they have enough money
+      stock_market.buy(ctx.message.author.id, stock.lower(), amount) # process purchase
+      await balance_redisplay() # refresh balance listings
+      await display_stocks(ctx.message.channel, ctx.message.author.id) # display users stocks
       await client.send_message(ctx.message.channel, "Done")
     else:
       await client.send_message(ctx.message.channel, "Not enough money, need " + str(stock_market.stock_prices[stock] * amount) + ", but only have " + str(bal))
   else:
     await client.send_message(ctx.message.channel, "Invalid stock name")
 
+# sell ALL stocks of a user
 @client.command(pass_context = True)
 async def sellall(ctx):
   for i in range(0, len(stocks.stock_data)):
     stock_name = list(stocks.stock_data.keys())[i]
     tmp = stock_market.get_agent_stock(ctx.message.author.id, stock_name)
     if tmp > 0:
-      await sell_stock(ctx, stock_name, tmp, display = False)
-  await balance_redisplay()
+      # dont display when selling individual stocks
+      await sell_stock(ctx, stock_name, tmp, display = False) # only sell stocks which the user owns
+  await balance_redisplay() # refresh balance listings
   await display_stocks(ctx.message.channel, ctx.message.author.id)
   await client.send_message(ctx.message.channel, "Done")
 
+# command to sell amount of stock
 @client.command(pass_context = True)
 async def sell(ctx, stock, amount):
   amount = int(amount)
   await sell_stock(ctx, stock, amount)
 
+# function to sell amount of stock
 async def sell_stock(ctx, stock, amount, display = True):
   if stock in stocks.stock_data.keys():
     u_amount = stock_market.get_agent_stock(ctx.message.author.id, stock)
     if u_amount >= amount:
       stock_market.sell(ctx.message.author.id, stock.lower(), amount)
       if display:
-        await balance_redisplay()
-        await display_stocks(ctx.message.channel, ctx.message.author.id)
+        await balance_redisplay() # refresh balance lsitings
+        await display_stocks(ctx.message.channel, ctx.message.author.id) # display users stocks
         await client.send_message(ctx.message.channel, "Done")
     else:
       if display:
@@ -113,27 +128,30 @@ async def sell_stock(ctx, stock, amount, display = True):
     if display:
       await client.send_message(ctx.message.channel, "Invalid stock name")
 
+# display stocks of a user
 async def display_stocks(channel, name):
   await client.send_message(channel, stock_market.get_agent_stock_str(name))
 
+# command to display own stocks
 @client.command(pass_context = True)
 async def mystocks(ctx):
   await display_stocks(ctx.message.channel, ctx.message.author.id)
 
+# initialises users and their balances
 @client.command(pass_context = True)
 async def init_balances(ctx, start_balance):
   start_balance = int(start_balance)
-  if not ctx.message.author.server_permissions.administrator:
+  if not ctx.message.author.server_permissions.administrator: # only for admins, otherwise exit
     return
-  users = ctx.message.server.members
+  users = ctx.message.server.members # get list of users
   stock_market.agents = []
   for user in users:
-    if user.id != "562318457799049236":
-      stock_market.add_agent(agent.Agent(str(user.id), start_balance, user.name))
+    if user.id != "562318457799049236": # (id of the bot)
+      stock_market.add_agent(agent.Agent(str(user.id), start_balance, user.name)) # add agent object for each user
 
-  await tick_redisplay()
+  await tick_redisplay() # initial news and stock display
   await client.send_message(ctx.message.channel, "Done")
-
+  
 @client.command(pass_context = True)
 async def bals(ctx): # display balances
   await balance_redisplay()
@@ -142,14 +160,16 @@ async def bals(ctx): # display balances
 async def mybal(ctx): # display balances
   await client.send_message(ctx.message.channel, str(stock_market.get_agent_balance(ctx.message.author.id)))
 
+# refresh balances
 async def balance_redisplay():
-  bal_channel = client.get_channel("563430326626549781")
-  await clear(bal_channel)
+  bal_channel = client.get_channel("563430326626549781") # (id of balance channel)
+  await clear(bal_channel) # clear channel before displaying
   string = ""
   for agent in stock_market.agents:
     string += agent.display_name + ": " + str(agent.balance) + " (" + str(stock_market.get_total_value(agent.owned_stocks) + agent.balance) + ")\n"
-  await client.send_message(bal_channel, string)
+  await client.send_message(bal_channel, string) # send string of all balances
 
+# refresh all displays
 async def tick_redisplay():
   news_channel = client.get_channel("562315190134112265")
   await clear(news_channel)
@@ -161,33 +181,35 @@ async def tick_redisplay():
 
   await balance_redisplay()
   
-
+# admin command to do next tick
 @client.command(pass_context = True)
 async def tick(ctx):
   if not ctx.message.author.server_permissions.administrator:
     return
-  stock_market.tick()
-  await tick_redisplay()
+  stock_market.tick() # process
+  await tick_redisplay() # refresh
   await client.send_message(ctx.message.channel, "Next turn @everyone")
 
+# user command to ready for next turn
 @client.command(pass_context = True)
 async def ready(ctx):
   await client.send_message(ctx.message.channel, "Readied")
-  if stock_market.ready_agent(ctx.message.author.id, True):
-    await tick_redisplay()
+  if stock_market.ready_agent(ctx.message.author.id, True): # returns true if all users are ready
+    await tick_redisplay() # refresh displays for next turn
     await client.send_message(ctx.message.channel, "Next turn @everyone")
 
+# user command to unready
 @client.command(pass_context = True)
 async def unready(ctx):
   await client.send_message(ctx.message.channel, "Unreadied")
-  stock_market.ready_agent(ctx.message.author.id, False)
-
+  stock_market.ready_agent(ctx.message.author.id, False) # set user unready status
+  
 @client.command(pass_context = True)
 async def du(ctx): # display unreadied
   string = "Unreadied:\n"
   for agent in stock_market.agents:
-    if not (agent.ready or agent.is_afk):
+    if not (agent.ready or agent.is_afk): # dont display if ready or afk
       string += agent.display_name + "\n"
   await client.send_message(ctx.message.channel, string)
 
-client.run(token)
+client.run(token) # runs the bot
