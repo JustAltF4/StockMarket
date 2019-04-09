8e7cc768d2fb69be02ed09bebd68e13d751afde3
JustAltF4 <35852992+JustAltF4@users.noreply.github.com>
2019-04-09 10:15:48 +0100

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,7 +1,18 @@
 
+RAND_FLUCT_MIN = 0.97
+RAND_FLUCT_MAX = 1.03
+
 # stock class
 class Stock():
-  def __init__(self, name, stock_type, country):
+  def __init__(self, name, price, stock_type, country):
     self.name = name
+    self.price = price
     self.stock_type = stock_type
     self.country = country
+    
+  def tick(self):
+    self.price *= random.uniform(RAND_FLUCT_MIN, RAND_FLUCT_MAX)
+    self.price = round(self.price)
+    if self.price <= 0:
+      self.price = 1
+    
